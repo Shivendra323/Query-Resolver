66dd5b9369cddd434f554c211831ad84d2e3fe6f
Abhinay Verma <aman.ver56@gmail.com>
2024-04-01 23:12:15 +0530

    Styled the card

--- client/src/Componenets/Post.jsx
+++ client/src/Componenets/Post.jsx
@@ -3,7 +3,7 @@ import { Card, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
 
-function Post({ username, content, initialComments }) {
+function Post({ username, content, imageUrl, initialComments }) {
   const [likes, setLikes] = useState(0);
   const [showComments, setShowComments] = useState(false);
   const [comments, setComments] = useState(initialComments);
@@ -20,9 +20,11 @@ function Post({ username, content, initialComments }) {
   };
 
   return (
-    <Card>
+    <Card className='mx-auto max-w-7xl h-auto my-3
+    transform transition duration-300 hover:scale-105 bg-white rounded-lg shadow-lg'>
       <Card.Header>{username}</Card.Header>
       <Card.Body>
+      {imageUrl && <img src={imageUrl} className='mb-2.5 w-full h-96'/>}
         <Card.Text>{content}</Card.Text>
       </Card.Body>
       <Card.Footer>
