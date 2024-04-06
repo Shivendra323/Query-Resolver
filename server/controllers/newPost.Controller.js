1961901244162c8c3dbb3eb865ef8b392d93185a
Abhinay Verma <aman.ver56@gmail.com>
2024-04-06 15:27:19 +0530

    changed Post Controller

--- server/controllers/newPost.Controller.js
+++ server/controllers/newPost.Controller.js
@@ -5,12 +5,21 @@ const newPost =  async(req, res) => {
     // Access form data here
     const user = await User.findOne({ username: req.body.username });
     const id = user._id;
-    const savePost = new Post({
-      content:req.body.userInput,
-      image:req.file.path,
-      user_id: id,
-    });
-
+    let savePost = null;
+    if(!req.file){
+      savePost = new Post({
+        content:req.body.userInput,
+        image:null,
+        user_id: id,
+      })
+    }
+    else{
+      savePost = new Post({
+        content:req.body.userInput,
+        image:req.file.path,
+        user_id: id,
+      });
+    }
     const data = await savePost.save();
     res.send("Success");
     return data;
